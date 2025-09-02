64dad94e7549051ffd687ef5a6f5b7249ffb7289
olegshady-ctrl <oleg.shady@gmail.com>
2025-09-03 02:09:57 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- eslint: { ignoreDuringBuilds: true },
-typescript: { ignoreBuildErrors: true },
+  // твои существующие настройки, например:
+  // reactStrictMode: true,
 
+  // ⬇️ чтобы билд не падал из-за ESLint/TS-ошибок
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
-
-
